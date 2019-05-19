17471d3fa40b93832adb30313fdebb9bdcd6deae
Sean McLean <seanm@apple.com>
2019-05-18 17:47:52 -0700

    screwin around

--- app/app.ts
+++ app/app.ts
@@ -1,8 +1,26 @@
-// lib/app.ts
-import express = require('express');
+import express = require( 'express' );
+import { Application } from 'express';
+import path = require( 'path' );
+import bodyParser = require( 'body-parser' );
+import http = require( 'http' );
+import os = require( 'os' );
+// import cookieParser from 'cookie-parser';
+// import logger from 'logger';
 
 // Create a new express application instance
 const app: express.Application = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
